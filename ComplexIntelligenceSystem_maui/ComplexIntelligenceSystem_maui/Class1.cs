b36bc993bc327a5533232e5dbc8afb710a7a53cf
EthanSystem <ElementsPhoenix@outlook.com>
2022-06-20 18:55:20 +0800

    init 2022-12-05
    新增.Net 7 MAUI项目；
    
    manage 2022-06-20 18:55:19
    
    Update CMakeLists.txt
    
    add 2022-06-20 21:55:49
    
    add 2022-06-23 01:33:24
    
    添加机器；
    
    Update main.cpp
    
    添加简易的运行主程序
    
    Update CMakeLists.txt
    
    Update BaseMachine.h
    
    构建id；
    
    mod 2022-06-24 02:21:00
    
    添加Object作为基类对象；
    添加接收端机器、发送端机器类对象；
    
    mod 2022-06-24 12:03:15
    
    mod 2022-06-25 01:30:15
    
    添加发送部件功能；
    
    add 2022-06-26 23:49:26
    
    mod 2022-06-27
    
    mod 2022-07-03 12:54:03
    
    mod 2022-07-04
    
    add 2022-07-04
    
    构建相关的基础组件，于python之版本；
    
    rename 2022-07-05
    
    重命名项目之Python包EntelechySystem为PyEntelechySystem；
    
    add 2022-07-05 21:29:19
    
    mod 2022-07-06 14:18:12
    
    构建基本初始化；
    
    change 2022-07-06
    
    add 2022-07-06 19:52:00
    
    继续建立Python相关基础件；
    
    Update main.py
    
    mod 2022-07-07 17:40:30
    
    管理相关文件和初始化方式；
    
    add 2022-07-07 17:40:48
    
    添加组件等；
    
    mod 2022-07-07 20:45:23
    
    继续构建基础件；
    
    mod 2022-07-08 22:17:32
    
    mod 2022-07-12 18:23:03
    
    mod 2022-07-12 20:34:14
    
    添加几个机；
    合并各单元文件为一个文件Units；
    管理导入包；
    
    rename 2022-07-25
    
    修改cpp版本之项目文件夹名为CppEntelechySystem；
    
    Create main.ts
    
    尝试使用TypeScript搭建系统；
    
    add 2022-07-30
    
    debug 2022-08-13
    
    修复错误；
    
    add 2022-08-13
    
    新建一些机器功能；
    
    manage 2022-08-13
    
    统计代码；
    
    Update main.py
    
    Create README.md
    
    Signed-off-by: Ethan Lin <ElementsPhoenix@outlook.com>
    
    mod 2022-08-14 00:55:05
    
    Update NeedMachine.py
    
    manage main_python 2022-09-27
    
    删除Python版本之非Python相关的主要的代码；
    
    manage 2022-08-15
    
    删除无用的idea相关文件；
    
    manage 2022-08-16 16:22:21
    
    删除代码统计数据文件；
    
    manage 2022-08-16 16:22:40
    
    重命名原有文件夹名、文件名、文件内容由「EntelechySystem」至「ComplexIntelligenceSystem」；
    
    manage 2022-08-16 23:30:31
    
    管理文件和文件夹；
    
    Create SceneBuilder.py

---
+++ ComplexIntelligenceSystem_maui/ComplexIntelligenceSystem_maui/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace ComplexIntelligenceSystem_maui
+{
+    // All the code in this file is included in all platforms.
+    public class Class1
+    {
+    }
+}
---
+++ PyComplexIntelligenceSystem/Core/Content/__init__.py
@@ -0,0 +1,9 @@
+"""
+@File   : __init__.py.py
+@Author : Ethan Lin
+@Date   : 2022/07/05
+@Desc   : 
+"""
+
+from PyComplexIntelligenceSystem.Core.Base import *
+from PyComplexIntelligenceSystem.Core.Content.Content import *
