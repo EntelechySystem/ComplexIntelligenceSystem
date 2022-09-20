7b24db6c88fbf9fcd571974b42ceb332d2e1d495
Ethan Lin <ElementsPhoenix@outlook.com>
2022-09-21 02:35:00 +0800

    Create SceneBuilder.py

---
+++ PyComplexIntelligenceSystem/World/SceneBuilder.py
@@ -0,0 +1,35 @@
+"""
+@File   : SceneBuilder.py
+@Author : Ethan Lin
+@Date   : 2022/09/20
+@Desc   : 大脑工厂构建工具，用于构建整个场景，暨工厂厂房。工厂厂房包括生产区、仓储区、进出口区、管理区等。
+"""
+
+from PyComplexIntelligenceSystem import np, dataclass
+from PyComplexIntelligenceSystem.Core.Base import *
+from PyComplexIntelligenceSystem.Core.Machine import *
+
+
+@dataclass()
+class SceneBuilder:
+    """
+    大脑工厂构建工具，用于构建整个场景，暨工厂厂房。
+    """
+
+    @staticmethod
+    def buildMachinesArray():
+        """构建机阵列"""
+        Machines = np.full((5, 5), BaseMachine)
+        # TODO阵列之间初始化随机位置，初始化随机连接
+        return Machines
+
+    @staticmethod
+    def buildDevicesArray():
+        """构建器阵列"""
+        Devices = np.full((5, 5), BaseDevice)
+        return Devices
+
+
+if __name__ == '__main__':
+    print(SceneBuilder.Machines)
+    print(SceneBuilder.Devices)
