3972749e6307134ca0271629d5eb25de944fa29a
Ethan Lin <ElementsPhoenix@outlook.com>
2022-11-04 20:07:58 +0800

    add 2022-11-04 20:08:10

---
+++ PyComplexIntelligenceSystem/Core/Device/LanguageStructureDevice.py
@@ -0,0 +1,14 @@
+"""
+@File   ：LanguageStructureDevice.py
+@Author ：Ethan Lin
+@Date   ：2022/11/04
+@Desc   : 语言结构器
+"""
+
+from PyComplexIntelligenceSystem import dataclass
+from PyComplexIntelligenceSystem.Core.Device import *
+
+
+def LanguageStructureDevice(BaseDevice):
+    # TODO
+    pass  # class
