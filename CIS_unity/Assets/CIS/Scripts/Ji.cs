3becf6ad6f25904b3361c17771c7bf1bc9f88d50
Ethan Lin <ElementsPhoenix@outlook.com>
2023-01-10 17:03:09 +0800

    add 2023-01-10 17:00:26
    新建`Ji`、`Qi`脚本并绑定到预制体；

---
+++ CIS_unity/Assets/CIS/Scripts/Ji.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CIS_unity
+{
+    public class Ji : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
