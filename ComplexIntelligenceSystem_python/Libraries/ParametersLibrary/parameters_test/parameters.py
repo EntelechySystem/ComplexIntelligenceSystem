1934e6712551e4855b3251cc6d8c621579e43112
Yee Cube <EthanSystem@outlook.com>
2024-10-19 12:03:33 +0800

    dev 开发实验框架
    修复程序预运行的一些问题，添加了一些说明；

--- ComplexIntelligenceSystem_python/Libraries/ParametersLibrary/parameters_test/parameters.py
+++ ComplexIntelligenceSystem_python/Libraries/ParametersLibrary/parameters_test/parameters.py
@@ -8,17 +8,6 @@ from engine.tools.Tools import Tools
 
 
 def main():
-    # %% 设置工作目录。
-    # folderpath_settings=Tools.setup_working_directory()
-    if Path(sys.argv[0]).name == Path(__file__).name:
-        # 在控制台运行，切换到脚本所在的文件夹
-        folderpath = Path(__file__).resolve().parent
-        os.chdir(folderpath)
-    else:
-        # 通过其他脚本运行，执行特定的代码
-        list_args = Tools.decode_args([*sys.argv[1:]])
-        folderpath = list_args[0]
-        pass  # if
 
     folderpath_parameters = folderpath
 
@@ -220,4 +209,15 @@ def main():
 
 
 if __name__ == '__main__':
+    # %% 设置工作目录。
+    # folderpath_settings=Tools.setup_working_directory()
+    if Path(sys.argv[0]).name == Path(__file__).name:
+        # 在控制台运行，切换到脚本所在的文件夹
+        folderpath = Path(__file__).resolve().parent
+        os.chdir(folderpath)
+    else:
+        # 通过其他脚本运行，执行特定的代码
+        list_args = Tools.decode_args([*sys.argv[1:]])
+        folderpath = list_args[0]
+        pass  # if
     main()
